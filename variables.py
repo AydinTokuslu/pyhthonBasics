9281c26416a5e2aa51565beb67496be35e2b9f5b
Aydın Tokuşlu <aydintokuslu78@gmail.com>
2022-08-30 10:23:26 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,42 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi = 0.27
+
+print(maasAli - (maasAli*vergi))
+print(maasAhmet - (maasAhmet*vergi))
+
+# değişken tanımlama kuralları
+# sayısal ifade rakam ile başlayamaz
+
+number1 = 10
+print(number1)
+number1 = 20
+print(number1)
+
+number1 += 30
+print(number1)
+
+# büyük küçük harf duyarlılığı vardır
+
+
+age = 20
+AGE = 30
+print(age)
+print(AGE)
+
+X = 1  # int
+Y = 2.3  # float
+NAME = "ÇINAR"  # string
+isStudent = True  # bool
+
+a = 10
+b = 20
+print(a+b)
+
+a = '10'
+b = '20'
+print(a+b)  # 1020
+
+firstname = "aydim"
+lastname = " tokuslu"
+print(firstname+lastname)
