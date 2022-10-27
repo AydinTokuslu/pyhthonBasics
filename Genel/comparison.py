2039fa53c00fa6bcca85ea95d147664f7c80c4ca
Aydın Tokuşlu <aydintokuslu78@gmail.com>
2022-10-27 23:30:09 +0300

    27.10.22

--- Genel/comparison.py
+++ Genel/comparison.py
@@ -1,12 +1,12 @@
 
-#1- girilen 2 sayıdan hangisi büyüktür?
+# 1- girilen 2 sayıdan hangisi büyüktür?
 #a=int(input("a sayısını giriniz : "))
 #b=int(input("b sayısını giriniz : "))
 
 #result= a > b
 #print(f'a: {a}, b: {b} den büyüktür : {result}')
 
-#2- kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.eğer 
+# 2- kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.eğer
 # ortalama 50 ve üstünde ise geçti, değilse kaldı yazdırın
 #vizeNotu1=float(input("1nci vize notunu giriniz : "))
 #vizeNotu2=float(input("2nci vize notunu giriniz : "))
@@ -15,24 +15,24 @@
 #ort = ((vizeNotu1+vizeNotu2)*0.60)/2 + (finalNotu*0.40)
 #print(f'not ortalamanız : {ort}, ve dersten geçme durumunuz : {ort>=50}')
 
-#3- girilen bir sayının tek mi çift mi olduğunuz yazdırın
+# 3- girilen bir sayının tek mi çift mi olduğunuz yazdırın
 #sayi=int(input("sayı giriniz : "))
-#tekCift=(sayi%2==0)
+# tekCift=(sayi%2==0)
 #print(f'girilen sayı çift olma durumu : {tekCift}')
-#4- girilen bir sayının negatif pozitif durumunu yazdırın
+# 4- girilen bir sayının negatif pozitif durumunu yazdırın
 #sayi=int(input("sayı giriniz : "))
-#pozitifMi=(sayi>0)
+# pozitifMi=(sayi>0)
 #print(f'girilen sayının pozitif olma durumu : {pozitifMi}')
 
-#5- parola ve email bilgisini isteyip doğruluğunu kontrol ediniz 
-# (email: email@sadikturan.com parola:abc123)
-email: 'mail@sadikturan.com' 
-password:'abc123'
+# 5- parola ve email bilgisini isteyip doğruluğunu kontrol ediniz
+# (email: emailmmmm@sadikturan.com parola:abc123)
+email: 'mailll@sadikturan.com'
+password: 'abc12345'
 
-girilenEmail=input('email : ')
-girilenPassword=input('password : ')
+girilenEmail = input('email : ')
+girilenPassword = input('password : ')
 
-isEmail = (email==girilenEmail.lower())
-isPassword = (password==girilenPassword.lower())
+isEmail = (email == girilenEmail.lower())
+isPassword = (password == girilenPassword.lower())
 
 print(f'Email bilgisi doğru mu : {isEmail} ve parola doğru mu : {isPassword}')
