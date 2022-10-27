cd662ca8990933189f30a90ba47ef174d06df71b
Aydın Tokuşlu <aydintokuslu78@gmail.com>
2022-10-27 23:08:19 +0300

    27.10.22

---
+++ AWS_Python Kursu/Hafta-1/ders2.py
@@ -0,0 +1,48 @@
+# my_age = 33
+# your_age = 30
+# my_age = your_age
+# print(my_age)
+
+# x = y = z = "same"
+# print(x)
+# print(y)
+# print(z)
+
+# january = march = may = july = august = october = december = 31
+# february = 28
+# april = june = september = november = 30
+# print(january, february, march, april, may, june, july,
+#       august, september, october, november, december)
+
+# x, y, z = "same", 5, 3.2
+# print(x)
+# print(y)
+# print(z)
+
+# y = "2.5"
+# print(float(y))
+
+# number_int = 123
+# number_flt = 1.23
+
+# number_new = number_int + number_flt
+
+# print("datatype of number_int:", type(number_int))
+# print("datatype of number_flt:", type(number_flt))
+
+# print("Value of number_new:", number_new)
+# print("datatype of number_new:", type(number_new))
+
+# number_int = 123
+# number_str = "456"
+
+# print("Data type of number_int:", type(number_int))
+# print("Data type of number_str before Type Casting:", type(number_str))
+
+# number_str = int(number_str)
+# print("Data type of number_str after Type Casting:", type(number_str))
+
+# number_sum = number_int + number_str
+
+# print("Sum of number_int and number_str:", number_sum)
+# print("Data type of the sum:", type(number_sum))
