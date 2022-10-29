0adfc1217f8bd8d6c842ff581ec9f2a50abc6e2b
Aydın Tokuşlu <aydintokuslu78@gmail.com>
2022-10-29 13:38:58 +0300

    29.10.22

--- AWS_Python Kursu/Hafta-1/ders3.py
+++ AWS_Python Kursu/Hafta-1/ders3.py
@@ -3,10 +3,25 @@
 # a = (1+3) ** (2 ** (1*2/2)/2)
 # print(a)           / bölme tipi ve sonucu float olur
 
-print("True" and "False")
-print(2 and "hello world")  # "hello world" döndürür
-print(2 or "hello world")  # "hello world" döndürür
-print("" and "hello world")  # boşluk döndürür
-print(None and ())  # None döndürür
-print({} or 0)  # F/F = F (or son değeri döndürür) 0 döndürür
-print({0} or False)  # T/F = T   {0} döndürür
+# print("True" and "False")
+# print(2 and "hello world")  # "hello world" döndürür
+# print(2 or "hello world")  # 2 döndürür
+# print("" and "hello world")  # boşluk döndürür
+# print(None and ())  # None döndürür
+# print({} or 0)  # F/F = F (or son değeri döndürür) 0 döndürür
+# print({0} or False)  # T/F = T   {0} döndürür
+
+nums = "123456789"
+print(nums[0:9:2])  # Tek Sayılar
+print(nums[1:9:2])  # Çift Sayılar
+
+print("clarus"+"way")
+
+a = "upper"
+b = " case"
+a += b  # a = a + b
+print(a)
+
+c = " letter"
+a += c  # a = a + c
+print(a)
