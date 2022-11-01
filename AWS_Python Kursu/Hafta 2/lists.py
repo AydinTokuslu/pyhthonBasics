88f662c506d8f79f88b516af9743038fbb09422b
aydin <aydintokuslu78@gmail.com>
2022-11-01 22:42:22 +0300

    01.11.22

--- AWS_Python Kursu/Hafta 2/lists.py
+++ AWS_Python Kursu/Hafta 2/lists.py
@@ -13,3 +13,9 @@ print(string2.lstrip("in"))
 text = 'tyou can learn almost everything in pre-classz'
 text = text.lstrip("t").rstrip("z").upper()
 print(text)
+
+list_1 = ['h', 'a', 'p', 'p', 'y']
+word = 'happy'
+list_2 = list(word)
+print(list_1)
+print(list_2)
