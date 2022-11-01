73bffff00eeca6e5b291206c494e8b98ff5037bd
aydin <aydintokuslu78@gmail.com>
2022-11-01 23:23:07 +0300

    01.11.22

--- AWS_Python Kursu/Hafta 2/lists.py
+++ AWS_Python Kursu/Hafta 2/lists.py
@@ -1,21 +1,26 @@
-sentence = "I live and work in Turkey"
-print(sentence.capitalize())
-print(sentence.replace("Turkey", "USA"))
-print(sentence.swapcase())
+# sentence = "I live and work in Turkey"
+# print(sentence.capitalize())
+# print(sentence.replace("Turkey", "USA"))
+# print(sentence.swapcase())
 
-string = "        interoperability    "
-string2 = "interoperability"
-print(string.strip())
-print(string.rstrip("ty"))
-print(string2.rstrip("ty"))
-print(string2.lstrip("in"))
+# string = "        interoperability    "
+# string2 = "interoperability"
+# print(string.strip())
+# print(string.rstrip("ty"))
+# print(string2.rstrip("ty"))
+# print(string2.lstrip("in"))
 
-text = 'tyou can learn almost everything in pre-classz'
-text = text.lstrip("t").rstrip("z").upper()
-print(text)
+# text = 'tyou can learn almost everything in pre-classz'
+# text = text.lstrip("t").rstrip("z").upper()
+# print(text)
 
-list_1 = ['h', 'a', 'p', 'p', 'y']
-word = 'happy'
-list_2 = list(word)
-print(list_1)
-print(list_2)
+# list_1 = ['h', 'a', 'p', 'p', 'y']
+# word = 'happy'
+# list_2 = list(word)
+# print(list_1)
+# print(list_2)
+
+bos_liste=[]
+bos_liste = list(range(1,5))
+print(bos_liste)
+
