0c737f59978ead8d4263fc7c1cf5c8b65261da3b
aydin <aydintokuslu78@gmail.com>
2022-11-03 21:19:33 +0300

    03.11.22

---
+++ AWS_Python Kursu/Hafta 2/tuple.py
@@ -0,0 +1,7 @@
+my_tuple = ("wakabayashi",)
+my_tuple2 = "wakabayashi"
+my_tuple2 = tuple("wakabayashi")
+print(my_tuple, sep="\n")
+print(type(my_tuple))
+print(type(my_tuple2))
+print(my_tuple2, sep="\n")
