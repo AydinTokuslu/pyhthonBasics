a06514f3520d780bd9f624eb2894f2b9314fd5db
aydin <aydintokuslu78@gmail.com>
2022-11-03 21:42:42 +0300

    03.11.22

--- AWS_Python Kursu/Hafta 2/tuple.py
+++ AWS_Python Kursu/Hafta 2/tuple.py
@@ -1,7 +1,12 @@
-my_tuple = ("wakabayashi",)
-my_tuple2 = "wakabayashi"
-my_tuple2 = tuple("wakabayashi")
-print(my_tuple, sep="\n")
-print(type(my_tuple))
-print(type(my_tuple2))
-print(my_tuple2, sep="\n")
+# my_tuple = ("wakabayashi",)
+# my_tuple2 = "wakabayashi"
+# my_tuple2 = tuple("wakabayashi")
+# print(my_tuple, sep="\n")
+# print(type(my_tuple))
+# print(type(my_tuple2))
+# print(my_tuple2, sep="\n")
+
+# my_tuple = (1, 2, 3, 4, 564, 7, 863)
+# my_list = list(my_tuple)
+# print(my_tuple, type(my_tuple))
+# print(my_list, type(my_list))
