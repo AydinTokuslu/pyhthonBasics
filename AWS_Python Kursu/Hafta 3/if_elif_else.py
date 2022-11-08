8a789e567f56aa40d0fa571bb3b91d74ce3c650c
aydin <aydintokuslu78@gmail.com>
2022-11-09 00:07:08 +0300

    09.11.22

---
+++ AWS_Python Kursu/Hafta 3/if_elif_else.py
@@ -0,0 +1,119 @@
+# if 0:
+#     print("This is true")
+# else:
+#     print("this is false")
+
+# x = 6
+# y = 9
+
+# print('is x equal to y?				:', x == y)
+# print('is x not equal to y?			:', x != y)
+# print('is x less than y?				:', x < y)
+# print('is x greater than y?			:', x > y)
+# print('is x less than or equal to y?		:', x <= y)
+# print('is x greater than or equal to y?		:', x >= y)
+
+# num1, num2 = int(input("iki sayı giriniz :"))
+# print(num1)
+# print(num2)
+
+# number = 49
+
+# if number >= 49:
+#     print(f"{number} is bigger than or equal to 72")
+# else:
+#     print(f"{number} is smaller than 72")
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print(f"{number} is even.")
+# else:
+#     print(f"{number} is odd.")
+
+# if number >= 0:
+#     print(f"{number} is positive number.")
+# else:
+#     print(f"{number} is negative number.")
+
+# a = int(input("Enter Number: "))
+# b = int(input("Enter Number: "))
+
+# if a > b:
+#     print(f"The large number is: {a}")
+# else:
+#     print(f"The large number is: {b}")
+
+# audience = "teen"
+
+# if audience == "kid":
+#     print("Free to go to cinema")
+# elif audience == "teen":
+#     print("discounted price")
+# elif audience == "adult":
+#     print("normal(x18.61) price")
+# else:
+#     print("No such audience.")
+
+# x = int(input("Enter a first number: "))
+# y = int(input("Enter a second number: "))
+# z = int(input("Enter a third number: "))
+
+# if x > y and x > z:
+#     print(x)
+# elif y > x and y > z:
+#     print(y)
+# elif z > y and z > x:
+#     print(z)
+# else:
+#     print("get lost")
+
+# num1 = float(input('Enter first number: '))
+# num2 = float(input('Enter second number: '))
+# num3 = float(input('Enter third number: '))
+
+# if (num1 >= num2) and (num1 >=num3):
+# 	largest = num1
+# elif (num2 >= num1) and (num2 >=num3):
+# 	largest = num2
+# else:
+# 	largest = num3
+
+# print('The largest number is', largest)
+
+# number = int(input("bir sayı giriniz:"))
+
+# if number > 0 :
+#      print(f"{number} bu sayı pozitif ")
+# elif number == 0 :
+#     print(f"{number} ne pozitif ne de negatiftir")
+# else:
+#      print(f"{number} bu sayı negatif")
+
+# audience_group = ['kid', 'teen', 'adult']
+# audience = "baby"
+
+# if audience in audience_group:
+#     if audience == "kid":
+#         print("Free to go to cinema")
+#     elif audience == "teen":
+#         print("discounted price")
+#     else: 
+#         print("normal price")
+# else:
+#     print("No such audience.")
+
+# score = int(input('Enter your score :'))
+
+# if score >=90:
+# 	if score >=95:
+# 		Score_letter='A+'
+# 	else:
+# 		Score_letter='A'
+# elif score >=80:
+# 	if score >=85:
+# 		Score_letter = 'B+'
+# 	else:
+# 		Score_letter = 'B'
+# else:
+# 	Score_letter = 'below B'
+# print(f"Your degree: {Score_letter}")
